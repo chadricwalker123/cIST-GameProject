17c0c703dbe7d6a5dad97d9793742de2a6e76324
chadricwalker123 <teekchoppa@gmail.com>
2026-02-27 19:22:39 -0600

    Added PlayerController script for B2 code assignment

---
+++ PlayerController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Start()
+    {
+        Debug.Log("Player controller added");
+    }
+}
